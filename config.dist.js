2c5b7a39e35e633b9173beb9241c50e6ab19e4cf
Xun Zhou <segentor@gmail.com>
2019-09-29 12:21:45 +0200

    updates

---
+++ config.dist.js
@@ -0,0 +1,5 @@
+export const config = {
+  user: '*',
+  password: '*',
+  host: 'ftp.strato.de'
+};
--- deploy.js
+++ deploy.js
@@ -1,20 +1,21 @@
+var ftpConfig = require('./config');
 var FtpDeploy = require('ftp-deploy');
 var ftpDeploy = new FtpDeploy();
 
 var config = {
-  user: '*', // NOTE that this was username in 1.x
-  password: '*', // optional, prompted if none given
-  host: 'ftp.strato.de',
+  user: ftpConfig.ftp.user, // NOTE that this was username in 1.x
+  password: ftpConfig.ftp.password, // optional, prompted if none given
+  host: ftpConfig.ftp.host,
   port: 21,
   localRoot: __dirname + '/api',
-  remoteRoot: '.',
+  remoteRoot: './api',
   include: ['*', '**/*'], // this would upload everything except dot files
   // include: ['*.php', 'dist/*'],
   exclude: ['build/**/*.map'], // e.g. exclude sourcemaps - ** exclude: [] if nothing to exclude **
   deleteRemote: false, // delete ALL existing files at destination before uploading, if true
   forcePasv: true // Passive mode is forced (EPSV command is not sent)
 };
-
+console.log(config);
 // use with promises
 ftpDeploy
   .deploy(config)
