16fee3c4b10c5f1330b64f17bdfb7940078920d4
Xun Zhou <segentor@gmail.com>
2019-09-29 13:22:18 +0200

    updates

--- api/SimpleJsonApi.php
+++ api/SimpleJsonApi.php
@@ -7,17 +7,31 @@ function handleRequest(string $output)
     header("Access-Control-Allow-Origin: *");
     header("Content-type: application/json");
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $jsonString = file_get_contents("php://input");
-        file_put_contents($output, $jsonString);
+        handlePOST($output);
+
+        return;
     }
 
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        handleGET($output);
+
+        return;
+    }
+}
+
+function handlePOST(string $output)
+{
+    $result = file_put_contents($output, file_get_contents("php://input"));
+
+    echo $result ? '200: OK' : '500: server error';
+}
+
+function handleGET(string $output)
+{
     if (file_exists($output)) {
         echo file_get_contents($output);
     } else {
         echo sprintf('Output file "%s" not found! Use POST to push some data at first.', $output);
-            
     }
 }
-
-
 
